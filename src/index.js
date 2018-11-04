cf63213972a71b37237800f1704ee97e04f6e75e
MartinKamenov <martinkamenov10@gmail.com>
2018-11-04 15:35:39 +0200

    Added navbar to app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+//import '../node_modules/bootstrap/dist/js/bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
