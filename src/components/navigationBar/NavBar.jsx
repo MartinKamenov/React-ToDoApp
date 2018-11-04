c7162bb04c7695caeb077ec91d199c2d4fcc92e9
MartinKamenov <martinkamenov10@gmail.com>
2018-11-04 15:53:29 +0200

    Added borders when hovering on navbar elements

--- src/components/navigationBar/NavBar.jsx
+++ src/components/navigationBar/NavBar.jsx
@@ -4,15 +4,18 @@ import './NavBar.css';
 const NavBar = () => {
     return ( 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand main_header" href="/">Navbar</a>
+            <a className="navbar-brand main_header main_header" href="/">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link navbar_element" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link navbar_element" href="/about">About</a>
                     </li>
                 </ul>
             </div>
