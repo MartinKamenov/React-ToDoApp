e1a54b1f2ca8b9ed89f3d6c597daa5b738173800
MartinKamenov <martinkamenov10@gmail.com>
2018-11-14 20:46:34 +0200

    Added navbar from bootstrap

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const About = () => {
     return ( 
         <div className="container">
-            <p>
-                This is an application written for testing purposes using ReactJS
-            </p>
+            <div className="nav-fragment">
+                <p>
+                    This is an application written for testing purposes using ReactJS
+                </p>
+            </div>
         </div>
     );
 }
--- src/components/navigationBar/NavBar.jsx
+++ src/components/navigationBar/NavBar.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+// import './NavBar.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
     return ( 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-fragment">
-            <span className="container">
-                <Link className="navbar-brand main_header main_header" to="/">Navbar</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link navbar_element" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link navbar_element" to="/about">About</Link>
-                        </li>
-                    </ul>
-                </div>
-            </span>
-        </nav>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <Link className="navbar-brand main_header main_header" to="/">Navbar</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle/>
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <Link className="nav-link navbar_element" to="/">Home</Link>
+                </Nav>
+                <Nav>
+                    <Link className="nav-link navbar_element" to="/about">About</Link>
+                </Nav>       
+            </Navbar.Collapse>
+        </Navbar>
      );
 }
  
